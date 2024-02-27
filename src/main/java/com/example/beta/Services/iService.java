5f16a8ab18d9fdfa41166b3c1413a5a1e287a0b6
Mohamed iheb ben dhaou <100792765+firsthunter@users.noreply.github.com>
2024-02-27 01:07:45 +0100

    Delete src directory

--- src/main/java/com/example/beta/Services/iService.java
+++
@@ -1,9 +0,0 @@
-package com.example.beta.Services;
-
-import java.util.Map;
-
-public interface iService<T> {
-    Map<String, Double> addUserNutrition(T t);
-    void deleteUserNutrition(int id);
-
-}
